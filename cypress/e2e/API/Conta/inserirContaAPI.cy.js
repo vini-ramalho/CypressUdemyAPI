f30e51c8a3f1f3e3ad69a501fb45703438fa86c6
viniramalho <alvesferreira.rvinicius@gmail.com>
2025-08-14 18:46:55 -0300

    Implementado o teste para validar o bloqueio de inserção de contas duplicadas

--- cypress/e2e/API/Conta/inserirContaAPI.cy.js
+++ cypress/e2e/API/Conta/inserirContaAPI.cy.js
@@ -13,10 +13,9 @@ beforeEach(() =>{
     cy.resetRest(token)
 })
 
-describe('Fazer uma requisição do tipo post para inserir uma conta', () =>{
+describe('Inserção de contas via API', () =>{
 
     it('Deve criar uma conta com sucesso', () =>{
-
                 cy.request({
                     method: 'POST',
                     headers:{Authorization: `JWT ${token}`},
@@ -30,6 +29,25 @@ describe('Fazer uma requisição do tipo post para inserir uma conta', () =>{
                 expect(res.status).to.be.equal(201) 
                 expect(res.body).to.have.property('id') 
                 expect(res.body).to.have.property('nome', 'Conta via rest')
+            })
+    })
+
+    it('Não deve permitir criar uma conta com nome duplicado', () =>{
+                        cy.request({
+                    method: 'POST',
+                    headers:{Authorization: `JWT ${token}`},
+                    url: '/contas',
+                    body:{
+                        nome:'Conta mesmo nome'
+                    },
+                    failOnStatusCode: false
+                }).as('response')
+
+            cy.get('@response').then(res => {
+                console.log(res)
+                expect(res.status).to.be.equal(400) 
+                expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
+                
             })
     })
 })
